e120b835c496b4050cc6b1a5a4df24b6104dc2b3
jaymin1088 <139453693+jaymin1088@users.noreply.github.com>
2023-10-14 22:18:24 +0530

    All files are uploaded
    All files are uploaded

--- weather_GL/JS/weather-utils.js
+++ weather_GL/JS/weather-utils.js
@@ -1,34 +1,29 @@
-
 class WeatherUtils {
-
-  static convertResponse(responseJSON){
-
+  static convertResponse(responseJSON) {
     const weatherResponse = {
-      LOCATION : `${responseJSON.name}, ${responseJSON.sys.country}`,
+      LOCATION: `${responseJSON.name}, ${responseJSON.sys.country}`,
       DATE: WeatherUtils.getTodayDate(),
-      TEMPERATURE : responseJSON.main.temp,
-      TEMPERATURE_TYPE : responseJSON.weather[0].main,
-      MIN_TEMPERATURE : responseJSON.main.temp_min,
-      MAX_TEMPERATURE : responseJSON.main.temp_max
-    }
+      TEMPERATURE: responseJSON.main.temp,
+      TEMPERATURE_TYPE: responseJSON.weather[0].main,
+      MIN_TEMPERATURE: responseJSON.main.temp_min,
+      MAX_TEMPERATURE: responseJSON.main.temp_max,
+    };
 
     return weatherResponse;
   }
 
-  static getTodayDate(){
-
+  static getTodayDate() {
     const today = new Date();
 
     const dateAsString = today.toLocaleDateString("en-US", {
       weekday: "long",
       year: "numeric",
       month: "long",
-      day: "numeric"
-    })
+      day: "numeric",
+    });
 
     return dateAsString;
   }
-
 }
 
-export {WeatherUtils}
+export { WeatherUtils };
